1ad0e7e341858b855d090c33a5067ab75a40d09a
beyazcreative <69454423+beyazcreative@users.noreply.github.com>
2020-08-27 14:17:14 +0300

    up new

---
+++ js/demo.js
@@ -0,0 +1,5 @@
+///// this for move by id in html page in all mobile and browser
+function MoveId() {
+    var elmntToView = document.getElementById(" for exapmle ID");
+    elmntToView.scrollIntoView();
+}
